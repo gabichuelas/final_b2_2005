3e62b26c4d05e05fc5a9ed1c5dfb8420563fb441
Gaby Mendez <gmendez90@gmail.com>
2020-08-03 12:09:39 -0400

    Add to flights#view, create flights#airline_name method

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Flight do
   end
 
   describe 'instance methods' do
-    it '#airline' do
-      @frontier = Airline.create(name: 'Frontier')
-      @flight1 = @frontier.flights.create(number: '1727', date: '08/03/20', time: '7:00 PM EST', departure: 'Denver', arrival: 'Reno')
-      
-      expect(@flight1.airline).to eq('Frontier')
+    it '#airline_name' do
+      frontier = Airline.create!(name: 'Frontier')
+      flight1 = frontier.flights.create!(number: '1727', date: '08/03/20', time: '7:00 PM EST', departure: 'Denver', arrival: 'Reno', airline_id: frontier.id)
+
+      expect(flight1.airline_name).to eq('Frontier')
     end
   end
 end
