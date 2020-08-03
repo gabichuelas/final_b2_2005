05e297ed22f25086d4e77921a316b67ab4b07274
Gaby Mendez <gmendez90@gmail.com>
2020-08-03 12:55:56 -0400

    Write Flight model specs for #count_of_adults and #count_of_minors

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -6,11 +6,34 @@ RSpec.describe Flight do
   end
 
   describe 'instance methods' do
-    it '#airline_name' do
-      frontier = Airline.create!(name: 'Frontier')
-      flight1 = frontier.flights.create!(number: '1727', date: '08/03/20', time: '7:00 PM EST', departure: 'Denver', arrival: 'Reno', airline_id: frontier.id)
+    before :each do
+      @frontier = Airline.create!(name: 'Frontier')
+      @flight1 = @frontier.flights.create!(number: '1727', date: '08/03/20', time: '7:00 PM EST', departure: 'Denver', arrival: 'Reno', airline_id: @frontier.id)
 
-      expect(flight1.airline_name).to eq('Frontier')
+      # test if you can do flight.passengers.create
+      @gaby = Passenger.create(name: 'Gaby', age: 30)
+      @robin = Passenger.create(name: 'Robin', age: 37)
+      @ruthie = Passenger.create(name: 'Ruthie', age: 27)
+      @child = Passenger.create(name: 'Strange Child', age: 12)
+      @young_adult = Passenger.create(name: 'I\'m Legal', age: 18)
+
+      FlightPassenger.create(flight_id: @flight1.id, passenger_id: @gaby.id)
+      FlightPassenger.create(flight_id: @flight1.id, passenger_id: @robin.id)
+      FlightPassenger.create(flight_id: @flight1.id, passenger_id: @ruthie.id)
+      FlightPassenger.create(flight_id: @flight1.id, passenger_id: @child.id)
+      FlightPassenger.create(flight_id: @flight1.id, passenger_id: @young_adult.id)
+    end
+
+    it '#airline_name' do
+      expect(@flight1.airline_name).to eq('Frontier')
+    end
+
+    it '#count_of_adults' do
+      expect(@flight1.count_of_adults).to eq('4')
+    end
+
+    it '#count_of_minors' do
+      expect(@flight1.count_of_minors).to eq('1')
     end
   end
 end
