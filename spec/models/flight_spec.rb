43077c7ab0808db7e926fe0b6770fa1281dac683
Gaby Mendez <gmendez90@gmail.com>
2020-08-03 11:46:29 -0400

    Write flight#airline method spec

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Flight do
     it { should have_many :flight_passengers }
     it { should have_many(:passengers).through(:flight_passengers)}
   end
+
+  describe 'instance methods' do
+    it '#airline' do
+      @frontier = Airline.create(name: 'Frontier')
+      @flight1 = @frontier.flights.create(number: '1727', date: '08/03/20', time: '7:00 PM EST', departure: 'Denver', arrival: 'Reno')
+      
+      expect(@flight1.airline).to eq('Frontier')
+    end
+  end
 end
