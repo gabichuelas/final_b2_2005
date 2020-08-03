0a208f103cb40e20e320272deaf6c79f217c6bee
Gaby Mendez <gmendez90@gmail.com>
2020-08-03 12:33:35 -0400

    Add spec for Add Flight form on passengers#show page

--- spec/features/passengers/show_spec.rb
+++ spec/features/passengers/show_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe 'As a visitor' do
 
   before :each do
     @frontier = Airline.create(name: 'Frontier')
+    @jetblue = Airline.create(name: 'JetBlue')
+
     @flight1 = @frontier.flights.create(number: '1727', date: '08/03/20', time: '7:00 PM MT', departure: 'Denver', arrival: 'Reno')
     @flight2 = @frontier.flights.create(number: '7777', date: '10/05/20', time: '4:00 PM EST', departure: 'Orlando', arrival: 'Denver')
     @flight3 = @frontier.flights.create(number: '7272', date: '10/10/20', time: '3:00 PM MT', departure: 'Denver', arrival: 'Orlando')
+
+    @flight4 = @jetblue.flights.create(number: '1234', date: '11/22/20', time: '5:00 PM EST', departure: 'Orlando', arrival: 'Santo Domingo')
 
     @gaby = Passenger.create(name: 'Gaby', age: 30)
 
@@ -36,6 +40,31 @@ RSpec.describe 'As a visitor' do
 
       click_on '7777'
       expect(current_path).to eq("/flights/#{@flight2.id}")
+    end
+
+    it 'I see a form to add a flight;
+    When I fill in the form with a flight number (assuming these will always be unique);
+    And click submit;
+    I\'m taken back to the passengers show page;
+    And I can see the flight number of the flight I just added' do
+      #
+      visit "/passengers/#{@gaby.id}"
+
+      within('.flight-list') do
+        expect(page).to_not have_content('1234')
+      end
+
+      select "#{@flight4.number}", :from => "flights"
+      click_on('Add Flight')
+
+      within('.flight-list') do
+        expect(page).to have_content('1234')
+      end
+
+      # no longer shows flight that was just added in the dropdown
+      within("#flights") do
+        expect(page).to_not have_content(@flight4.number)
+      end
     end
   end
 end
