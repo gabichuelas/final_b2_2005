36f91dfcd729b045d1f0f51b913ad828960a23f7
Gaby Mendez <gmendez90@gmail.com>
2020-08-03 13:03:28 -0400

    Add seeds for fun

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 @frontier = Airline.create(name: 'Frontier')
-@flight1 = @frontier.flights.create(number: '1727', date: '08/03/20', time: '7:00 PM EST', departure: 'Denver', arrival: 'Reno')
+@jetblue = Airline.create(name: 'JetBlue')
+
+@flight1 = @frontier.flights.create(number: '1727', date: '08/03/20', time: '7:00 PM MT', departure: 'Denver', arrival: 'Reno')
+@flight2 = @frontier.flights.create(number: '7777', date: '10/05/20', time: '4:00 PM EST', departure: 'Orlando', arrival: 'Denver')
+@flight3 = @frontier.flights.create(number: '7272', date: '10/10/20', time: '3:00 PM MT', departure: 'Denver', arrival: 'Orlando')
+
+@flight4 = @jetblue.flights.create(number: '1234', date: '11/22/20', time: '5:00 PM EST', departure: 'Orlando', arrival: 'Santo Domingo')
 
 @gaby = Passenger.create(name: 'Gaby', age: 30)
 @robin = Passenger.create(name: 'Robin', age: 37)
 @ruthie = Passenger.create(name: 'Ruthie', age: 27)
 @child = Passenger.create(name: 'Strange Child', age: 12)
+@young_adult = Passenger.create(name: 'I\'m Legal', age: 18)
 
 FlightPassenger.create(flight_id: @flight1.id, passenger_id: @gaby.id)
 FlightPassenger.create(flight_id: @flight1.id, passenger_id: @robin.id)
 FlightPassenger.create(flight_id: @flight1.id, passenger_id: @ruthie.id)
 FlightPassenger.create(flight_id: @flight1.id, passenger_id: @child.id)
+FlightPassenger.create(flight_id: @flight1.id, passenger_id: @young_adult.id)
