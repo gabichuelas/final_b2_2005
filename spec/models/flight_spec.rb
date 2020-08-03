e0f117c9013ed5d34f057b5543f4846f9edcfbf9
Gaby Mendez <gmendez90@gmail.com>
2020-08-03 11:23:06 -0400

    Write Flight model spec

---
+++ spec/models/flight_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe Flight do
+  describe 'relationships' do
+    it { should belong_to :airline }
+    it { should have_many :flight_passengers }
+    it { should have_many :passengers, through: :flight_passengers}
+  end
+end
